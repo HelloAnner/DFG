c93431fe0df7d41e9c84d75249c61c2f50f1a8a9
Anner <anner@fanruan.com>
2021-10-10 12:13:37 +0800

    feat: add adaptor pattern

---
+++ pattern/adaptor/adaptor.go
@@ -0,0 +1,43 @@
+package adaptor
+
+//Target 是适配的目标接口
+type Target interface {
+	Request() string
+}
+
+// ==========实际的方法 =============
+
+type Adaptee interface {
+	SpecificRequest() string
+}
+
+func NewAdaptee() Adaptee {
+	return &adapteeImpl{}
+}
+
+type adapteeImpl struct {
+}
+
+func (*adapteeImpl) SpecificRequest() string {
+	return "adaptee method"
+}
+
+// ==================================
+
+// ==================转换器===================
+
+type Adapter struct {
+	Adaptee
+}
+
+func NewAdapter(adaptee Adaptee) Target {
+	return &Adapter{
+		Adaptee: adaptee,
+	}
+}
+
+func (a *Adapter) Request() string {
+	return a.SpecificRequest()
+}
+
+// ===========================================
