89a2f801e5becb6115cea267cceb7c8a126001e1
Anner <anner.wang@icloud.com>
2023-07-15 19:55:05 +0800

    每日一题

---
+++ leetcode_demo/leetcode/editor/cn/[103]二叉树的锯齿形层序遍历.go
@@ -0,0 +1,89 @@
+//给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[20,9],[15,7]]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1]
+//输出：[[1]]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 2000] 内
+// -100 <= Node.val <= 100
+//
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 787 👎 0
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+// 这里的返回值有点意思
+// 主要是学习一下层序遍历的过程
+func zigzagLevelOrder(root *TreeNode) (ans [][]int) {
+	if root == nil {
+		return
+	}
+	queue := []*TreeNode{root}
+	// 直接先层序便利
+	for level := 0; len(queue) > 0; level++ {
+		vals := []int{}
+		q := queue
+		queue = nil // golang 的神奇特色
+
+		for _, node := range q {
+			vals = append(vals, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		// 开始逆转一下顺序
+		if level%2 == 1 {
+			for i, n := 0, len(vals); i < n/2; i++ {
+				vals[i], vals[n-1-i] = vals[n-1-i], vals[i]
+			}
+		}
+		ans = append(ans, vals)
+	}
+	return
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode_demo/leetcode/editor/cn/[200]岛屿数量.go
@@ -0,0 +1,75 @@
+// 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+//
+// 此外，你可以假设该网格的四条边均被水包围。
+//
+// 示例 1：
+//
+// 输入：grid = [
+//
+//	["1","1","1","1","0"],
+//	["1","1","0","1","0"],
+//	["1","1","0","0","0"],
+//	["0","0","0","0","0"]
+//
+// ]
+// 输出：1
+//
+// 示例 2：
+//
+// 输入：grid = [
+//
+//	["1","1","0","0","0"],
+//	["1","1","0","0","0"],
+//	["0","0","1","0","0"],
+//	["0","0","0","1","1"]
+//
+// ]
+// 输出：3
+//
+// 提示：
+//
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 300
+// grid[i][j] 的值为 '0' 或 '1'
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 2219 👎 0
+package main
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func numIslands(grid [][]byte) (ans int) {
+	// 二维数组的 DFS 模板参考
+	m, n := len(grid), len(grid[0])
+
+	var dfs func(r, c int)
+	dfs = func(r, c int) {
+		if r < 0 || r >= m || c < 0 || c >= n {
+			return
+		}
+
+		if grid[r][c] != '1' {
+			return
+		}
+
+		grid[r][c] = '2'
+		dfs(r-1, c)
+		dfs(r+1, c)
+		dfs(r, c+1)
+		dfs(r, c-1)
+	}
+
+	for r := 0; r < m; r++ {
+		for c := 0; c < n; c++ {
+			if grid[r][c] == '1' { // 遇到一个陆地，结果+1，并则将其周围四个相连的陆地变为非陆地
+				ans++
+				dfs(r, c)
+			}
+		}
+	}
+
+	return
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
